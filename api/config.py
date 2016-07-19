304d486009b267f1aec541b23da416b8822ccde1
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-07-19 10:33:03 -0700

    Add flask app, add config fields.

--- api/config.py
+++ api/config.py
@@ -3,12 +3,12 @@ import os
 
 status = os.environ.get("COMMANDO_PROJECT_ENV")
 
-DEBUG =
-HOST =
-PORT =
-DATABASE = {"host": "",
-            "user": "",
-            "database": "",
-            "port": ,
-            "charset": "",
-            "password": ""}
+DEBUG = True
+HOST = "localhost"
+PORT = 3333
+DATABASE = {"host": "172.31.48.131",
+            "user": "root",
+            "database": "commando_database",
+            "port": 3306,
+            "charset": "utf8",
+            "password": "root"}
