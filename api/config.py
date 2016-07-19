e92a3e23585e50fbecf14e95d16fc8ccd337a489
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-07-19 10:26:46 -0700

    Add application file structure.

---
+++ api/config.py
@@ -0,0 +1,14 @@
+'''Add MySQL database configurations.'''
+import os
+
+status = os.environ.get("COMMANDO_PROJECT_ENV")
+
+DEBUG =
+HOST =
+PORT =
+DATABASE = {"host": "",
+            "user": "",
+            "database": "",
+            "port": ,
+            "charset": "",
+            "password": ""}
